0f84b72dc31f0df677be7da968cda566291ac885
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-03-09 18:44:50 +0800

    gourmap-util: Restore json_node_from_call()

--- src/gourmap-util.c
+++ src/gourmap-util.c
@@ -1,3 +1,55 @@
 #include <libsoup/soup.h>
+#include <rest/rest-proxy.h>
+#include <json-glib/json-glib.h>
+
 #include "gourmap-util.h"
 
+JsonNode *
+json_node_from_call (RestProxyCall *call)
+{
+	JsonParser *parser;
+	JsonNode *root = NULL;
+	JsonObject *obj = NULL;
+	GError *error;
+	gboolean ret = FALSE;
+	const char *status;
+
+	parser = json_parser_new ();
+
+	if (call == NULL)
+		goto out;
+
+	if (!SOUP_STATUS_IS_SUCCESSFUL (rest_proxy_call_get_status_code (call))) {
+		g_message ("Error from Google Map: %s (%d)",
+			   rest_proxy_call_get_status_message (call),
+			   rest_proxy_call_get_status_code (call));
+		goto out;
+	}
+
+	ret = json_parser_load_from_data (parser,
+					  rest_proxy_call_get_payload (call),
+					  rest_proxy_call_get_payload_length (call),
+					  &error);
+	root = json_parser_get_root (parser);
+
+	if (root == NULL) {
+		g_message ("Error from Google Map: %s",
+		rest_proxy_call_get_payload (call));
+		goto out;
+	}
+
+	/* check status */
+	obj = json_node_get_object (root);
+	status = json_object_get_string_member (obj, "status");
+	if (g_strcmp0 (status, "OK") != 0) {
+		g_message ("Error from Google Map: %s", status);
+		root = NULL;
+		goto out;
+	}
+
+	root = json_node_copy (root);
+out:
+	g_object_unref (parser);
+
+	return root;
+}
--- src/gourmap-util.h
+++ src/gourmap-util.h
@@ -1,4 +1,10 @@
 #ifndef __GOURMAP_UTIL_H__
 #define __GOURMAP_UTIL_H__
 
+#include <libsoup/soup.h>
+#include <rest/rest-proxy.h>
+#include <json-glib/json-glib.h>
+
+JsonNode	*json_node_from_call	(RestProxyCall	*call);
+
 #endif /* __GOURMAP_UTIL_H__ */
