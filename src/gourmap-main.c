3d7e6ac8d830b8b17fdd2a66268fe7944594885d
Gary Ching-Pang Lin <chingpang@gmail.com>
2011-03-10 12:11:41 +0800

    gourmap-main: Add command line option for POI database

--- src/gourmap-main.c
+++ src/gourmap-main.c
@@ -12,6 +12,14 @@ int
 main (int argc, char **argv)
 {
 	GourmapCoord *coord;
+	const char *poi_db_filename;
+	GOptionContext *context;
+	const GOptionEntry options[] = {
+		{ "import", 'i', 0, G_OPTION_ARG_STRING, &poi_db_filename,
+		  /* TRANSLATORS: use another config file instead of the default one */
+		  _("Import a POI database"), NULL },
+		{ NULL }
+	};
 
 #ifdef ENABLE_NLS
 	bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
@@ -21,7 +29,13 @@ main (int argc, char **argv)
 
 	gtk_init (&argc, &argv);
 
+	context = g_option_context_new ("Gourmap");
+	g_option_context_add_main_entries (context, options, NULL);
+	g_option_context_parse (context, &argc, &argv, NULL);
+	g_option_context_free (context);
+
 	coord = gourmap_coord_new ();
+	gourmap_coord_import_poi_db (coord, poi_db_filename);
 
 	gtk_main ();
 
